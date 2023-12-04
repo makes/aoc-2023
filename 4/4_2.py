dc2ef5c9f82cc17205cad590eeb3ead318dffd68
makes <1053209+makes@users.noreply.github.com>
2023-12-04 14:06:03 +0200

    remove unused variable

--- 4/4_2.py
+++ 4/4_2.py
@@ -1,27 +1,26 @@
 with open("input.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
 
-card_info = {}
+cards = {}
 
 for line in lines:
     idstr, numstr = line.split(':')
     mynums, winningnums = numstr.split('|')
     cardid = int(idstr.split()[1])
-    card_info[cardid] = [[int(x) for x in mynums.split()],
+    cards[cardid] = [[int(x) for x in mynums.split()],
                          [int(x) for x in winningnums.split()]]
 
 def count_matches(cardid):
-    mynums = card_info[cardid][0]
-    winningnums = card_info[cardid][1]
+    mynums = cards[cardid][0]
+    winningnums = cards[cardid][1]
     matches = 0
     for num in mynums:
         if num in winningnums:
             matches += 1
     return matches
 
-matches = {cid: count_matches(cid) for cid in card_info}
+matches = {cid: count_matches(cid) for cid in cards}
 
-cards = {}
 def count_cards(ids):
     n = 0
     for cardid in ids:
@@ -31,5 +30,5 @@ def count_cards(ids):
             n += count_cards(list(range(cardid + 1, cardid + m + 1)))
     return n
 
-print(count_cards(card_info))
+print(count_cards(cards))
 
