0ec4a52fe1149e976fea257ed1fbb5e042230810
makes <1053209+makes@users.noreply.github.com>
2023-12-07 22:21:20 +0200

    improve

--- 4/4_2.py
+++ 4/4_2.py
@@ -1,15 +1,3 @@
-with open("input.txt", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-
-cards = {}
-
-for line in lines:
-    idstr, numstr = line.split(':')
-    mynums, winningnums = numstr.split('|')
-    cardid = int(idstr.split()[1])
-    cards[cardid] = [[int(x) for x in mynums.split()],
-                         [int(x) for x in winningnums.split()]]
-
 def count_matches(cardid):
     mynums = cards[cardid][0]
     winningnums = cards[cardid][1]
@@ -19,16 +7,30 @@ def count_matches(cardid):
             matches += 1
     return matches
 
-matches = {cid: count_matches(cid) for cid in cards}
 
-def count_cards(ids):
+def count_cards(ids, winnings={}):
     n = 0
     for cardid in ids:
-        m = matches[cardid]
-        n += 1
-        if m > 0:
-            n += count_cards(list(range(cardid + 1, cardid + m + 1)))
+        if cardid not in winnings:
+            m = count_matches(cardid)
+            winnings[cardid] = list(range(cardid + 1, cardid + m + 1))
+        n += 1 + count_cards(winnings[cardid], winnings)
     return n
 
-print(count_cards(cards))
 
+if __name__ == "__main__":
+    with open("input.txt", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    cards = {}
+
+    for line in lines:
+        idstr, numstr = line.split(":")
+        mynums, winningnums = numstr.split("|")
+        cardid = int(idstr.split()[1])
+        cards[cardid] = [
+            [int(x) for x in mynums.split()],
+            [int(x) for x in winningnums.split()],
+        ]
+
+    print(count_cards(cards))
